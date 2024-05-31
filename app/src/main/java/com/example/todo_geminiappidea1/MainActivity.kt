57af9ca99cf4cb12a2ba1b3e0f7ef14981fe353a
Nj <92800887+omarndungo@users.noreply.github.com>
2024-05-31 21:47:19 +0300

    Added An extended FAB for adding tasks.

--- app/src/main/java/com/example/todo_geminiappidea1/MainActivity.kt
+++ app/src/main/java/com/example/todo_geminiappidea1/MainActivity.kt
@@ -4,30 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.todo_geminiappidea1.core.AddTask
-import com.example.todo_geminiappidea1.core.DeleteTask
-import com.example.todo_geminiappidea1.core.EditTask
-import com.example.todo_geminiappidea1.core.MarkTaskComplete
 import com.example.todo_geminiappidea1.core.TaskList
 import com.example.todo_geminiappidea1.data.Task
-import com.example.todo_geminiappidea1.ui.theme.Todo_GeminiAppIdea1Theme
+import com.example.todo_geminiappidea1.ui.theme.Purple40
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,9 +43,23 @@ fun Todo_GeminiAppIdea1() {
         Task(5, "Go to the gym", "2024-08-20", false)
     )
 
-    TaskList(tasks = tasks)
-    //TODO: Find out how to add a floating action button to call the AddTask function
-    //AddTask(onAddTask = {})
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        TaskList(tasks = tasks)
+        //TODO: Find out how to add a floating action button to call the AddTask function
+        //AddTask(onAddTask = {})
+        CreateTask(onClick = {
+            /*AddTask(
+                onAddTask = {"Task Added"},
+
+            )*/
+        })
+    }
+
+
+
 
     /*EditTask(task = tasks[0], onEditTask = {})
     DeleteTask(task = tasks[2], onDeleteTask = {})
@@ -62,4 +67,19 @@ fun Todo_GeminiAppIdea1() {
 
     // Ugly stuff - let's work on the UI next!!!
 
+}
+
+@Composable
+fun CreateTask(onClick: @Composable () -> Unit) {
+    // TODO Create a Floating action button for the above page
+
+    ExtendedFloatingActionButton(
+        onClick = {},
+        containerColor = Purple40,
+        contentColor = Color.White,
+        text = { Text("Add Task") },
+        icon = { Icons.Filled.Add },
+        modifier = Modifier,
+        elevation = FloatingActionButtonDefaults.elevation()
+    )
 }
